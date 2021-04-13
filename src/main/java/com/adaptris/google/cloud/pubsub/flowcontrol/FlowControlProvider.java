eb9b33e5bf7bb3d49eb8995eeaa02c4d54954c18
Sebastien Belin <sebastien.belin@adaptris.com>
2021-04-13 02:51:02 +0100

    Remove code warnings and use of deprecated methods.

--- src/main/java/com/adaptris/google/cloud/pubsub/flowcontrol/FlowControlProvider.java
+++ src/main/java/com/adaptris/google/cloud/pubsub/flowcontrol/FlowControlProvider.java
@@ -3,7 +3,6 @@ package com.adaptris.google.cloud.pubsub.flowcontrol;
 import com.adaptris.core.ComponentLifecycle;
 import com.adaptris.core.CoreException;
 import com.google.cloud.pubsub.v1.Subscriber;
-import com.thoughtworks.xstream.annotations.XStreamAlias;
 
 /**
  * @author mwarman
